0ea29b09c9f60e87a38676d2b45d2884e97e355f
Shahab Ali <shali@macrosoftinc.com>
2024-07-08 14:53:51 +0500

    working on make payment form

--- AshreiTech_ReactJs/src/screens/AsheriTechComponents/RegistrationForm/MakePayment.js
+++ AshreiTech_ReactJs/src/screens/AsheriTechComponents/RegistrationForm/MakePayment.js
@@ -1,13 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import "../AdmissionEnquiery/AdmissionEnquiery.css";
-import { Checkbox, FormControl, FormControlLabel, InputLabel, Select, TextField } from '@mui/material/node';
+import "./RegisterYourself.css"
+import { FormControl, InputLabel, Select, TextField } from '@mui/material/node';
 const INITIAL_STATE = {
     fname: "",
     lname: "",
 }
+const dowpdownData = [
+    { Id: 1, stxt: "value1" },
+    { Id: 2, stxt: "value2" },
+    { Id: 3, stxt: "value3" }
+]
 export const MakePayment = (props) => {
     const [formData, setFormData] = useState({ ...INITIAL_STATE })
-    
+
 
 
     return (
@@ -26,19 +32,44 @@ export const MakePayment = (props) => {
                                 <TextField
                                     className="w-100"
                                     id="outlined-controlled"
-                                    label="First Name"
+                                    label="Payment No"
                                     size="small"
                                     name="fname"
                                 />
                             </div>
                             <div className='col-lg-6'>
-                                <TextField
-                                    className="w-100"
-                                    id="outlined-controlled"
-                                    label="Last Name"
-                                    size="small"
-                                    name="lname"
-                                />
+                                <FormControl variant="outlined" size="small" className='w-100'>
+                                    <InputLabel htmlFor="outlined-age-native-simple">Payment Type</InputLabel>
+                                    <Select
+                                        name="gender"
+                                        native
+                                        label="Payment Type"
+                                        inputProps={{
+                                            name: 'gender',
+                                            id: 'outlined-age-native-simple',
+                                        }}
+                                    >
+                                        <option value={0}></option>
+                                        {
+                                            dowpdownData && dowpdownData?.map((Val, index) => {
+                                                return (<option key={index} value={Val.Id}>{Val.stxt}</option>)
+                                            })
+                                        }
+
+                                    </Select>
+                                </FormControl>
+                            </div>
+                        </div>
+                        <div className='row mt-3'>
+                            <div className='col-lg-12 d-flex justify-content-end'>
+                                <button type="button" className="generateBtn">Generate</button>
+                            </div>
+                        </div>
+
+                        <div className='row mt-5'>
+                            <div className='col-lg-12 d-flex justify-content-end'>
+                                <button className='buttonClass mr-2' onClick={(event) => props?.handleChange(event, 0)}> Previous</button>
+                                <button type='submit' className='buttonClass' onClick={(event) => props?.handleChange(event, 2)}> Next</button>
                             </div>
                         </div>
 
