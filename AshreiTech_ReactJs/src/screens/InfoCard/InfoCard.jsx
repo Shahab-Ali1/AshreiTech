2d194adf11c8537792dd25dc52893603d9561e09
Zain Khan <zainkhan0453@gmail.com>
2024-07-24 12:34:37 +0500

    update ui of course page

--- AshreiTech_ReactJs/src/screens/InfoCard/InfoCard.jsx
+++ AshreiTech_ReactJs/src/screens/InfoCard/InfoCard.jsx
@@ -5,7 +5,6 @@ import ScrollTrigger from 'react-scroll-trigger';
 
 
 const InfoCard = (props) => {
-
     const { Info_container, vector, gradientCircle } = style;
     const [counter, setCounter] = useState(false)
 
@@ -13,18 +12,18 @@ const InfoCard = (props) => {
         <>
             <ScrollTrigger onEnter={() => setCounter(true)} onExit={() => setCounter(false)}>
                 <div className={`${Info_container}  p-1 mt-5 mx-lg-5`}>
-                    <div className={`${gradientCircle} d-none d-lg-block `} style={{position:'relative', zIndex:'-2'}}></div>
-                    <div className={`row d-flex  ${props?.data?.length == 4 ? 'px-5 py-3 justify-content-between' : "justify-content-center"}  align-items-center  pt-4`}>
+                    {/* <div className={`${gradientCircle} d-none d-lg-block `} style={{position:'relative', zIndex:'-2'}}></div> */}
+                    <div className={` d-flex  ${props?.data?.length == 4 ? 'px-5  justify-content-between' : "justify-content-center"}  align-items-center  pt-4`}>
                         {
                             props?.data?.map(({ image, heading, discription }) => {
                                 const updatedDescription = discription.replace(/[K+]/g, '');
                                 return <div className={` ${props?.data?.length == 4 ? '' : "col-12 col-md-3 col-lg-2 "}  d-flex align-items-center justify-content-center mb-3`}>
                                     <div>
-                                        <img src={image} className={`${vector} mr-2`} style={{ width: '44px', height: '44px' }} alt="Students" />
+                                        <img src={image} className={`${vector} mr-2`} style={{ width: '65%',}} alt="Students" />
                                     </div>
-                                    <div>
-                                        {!props.count && <h6 style={{ fontWeight: 'bold' }}>{discription}</h6>}
+                                    <div className='ml-n3'>
                                         <h6 style={{ fontWeight: '' }}>{heading}</h6>
+                                        {!props.count && <h6 style={{ fontWeight: 'bold' }}>{discription}</h6>}
                                         {
                                             props.count && counter &&
                                             <h6 style={{ fontWeight: 'bold' }}> <Countup
@@ -39,6 +38,10 @@ const InfoCard = (props) => {
                         }
 
                     </div>
+                       {/* Circle with Gradient */}
+            {
+                props?.gradient == false ? '' :<div className={`${gradientCircle} d-none d-lg-block `}></div>
+            }
                 </div>
             </ScrollTrigger>
 
