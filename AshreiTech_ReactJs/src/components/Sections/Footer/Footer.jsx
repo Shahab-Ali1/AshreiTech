6aa8a80fbbcdea377ea5c286decdd9aaa078293a
ainulislam <auislam@macrosoftinc.com>
2024-07-31 21:21:17 +0500

    footer changes

--- AshreiTech_ReactJs/src/components/Sections/Footer/Footer.jsx
+++ AshreiTech_ReactJs/src/components/Sections/Footer/Footer.jsx
@@ -6,16 +6,17 @@ import Map from "../../../assets/map.png";
 import PhoneIcon from "../../../assets/PhoneIcon.png";
 import pakistanIcon from "../../../assets/pakistanIcon.png";
 import mosqueIcon from "../../../assets/mosqueIcon.png";
+import wmap from "../../../assets/wmap.png";
 import FedumanLogo from "../../../assets/FeduManLogo.png";
 
 const Footer = () => {
     const { footer_container, containerOne, containerOnePiece, containerTwo } = style;
     return (
-        <div className={`${footer_container} container-fluid  px-3 px-md-5 pt-0 pt-md-5 `}>
+        <div className={`${footer_container} container-fluid  px-3 px-md-5 pt-0 pt-md-5`}>
             <div className="row px-0 px-md-5 py-0 py-md-5">
                 <div className="col-lg-6 col-md-6 col-sm-12 ">
                     <div className="row pb-4 mb-4 pt-1 pl-0 pl-lg-3 d-flex justify-content-center justify-content-lg-start">
-                        <img src={AshrieTechWhiteLogo} alt="AshrieTech Logo" className="pt-5 pt-lg-0" style={{width:'60%', height:'auto'}}/>
+                        <img src={AshrieTechWhiteLogo} alt="AshrieTech Logo" className="pt-5 pt-lg-0" style={{width:'40%', height:'auto'}}/>
                     </div>
                     <div className="row d-flex justify-content-center justify-content-lg-start ">
                         <div className="col-3">
@@ -44,48 +45,58 @@ const Footer = () => {
                         </div>
                     </div>
                 </div>
-                {/* <div className="col-lg-6 col-md-6 col-sm-12 d-flex flex-column justify-content-center align-items-center"> */}
-                <div className="col-lg-6 col-md-6 col-sm-12">
+                <div className="col-lg-6 col-md-6 col-sm-12 "
+                style={{
+                    backgroundImage: `url(${wmap})`,
+                    backgroundRepeat: 'no-repeat',
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    width:'100%',
+                    height:'auto',
+                    
+                }}
+                >
                     <div className='row'>
                         <div className='col-lg-12'><h3>Our Branches</h3></div>
                     </div>
-                    <div className="row mt-5">
+                    <div className="row mt-5 d-flex justify-content-center align-items-center">
+
                         <div className='col-lg-4'>
-                            <div style={{ color: '#DBDBDB' }} className="d-flex pb-3">
-                                <img src={Minar} alt="minar" className="pt-5 pt-lg-0" style={{height: "35px"}} />
-                                <span className='mt-3'> Lahore Branch </span>
+                            <div style={{ color: '#DBDBDB' }} className="d-flex justify-content-center align-items-baseline">
+                                <img src={Minar} alt="minar" className="mx-2" style={{height: "35px"}} />
+                                <span className=''> Lahore Branch </span>
                             </div>
                         </div>
                         <div className='col-lg-4'>
-                            <div style={{ color: '#DBDBDB' }} className="d-flex pb-3">
-                                <img src={pakistanIcon} alt="pakistanIcon" className="pt-5 pt-lg-0" style={{height: "27px"}}/>
-                                <span className='mt-1'>Karachi Branch</span>
+                            <div style={{ color: '#DBDBDB' }} className="d-flex justify-content-center align-items-baseline">
+                                <img src={pakistanIcon} alt="pakistanIcon" className="mx-2" style={{height: "35px"}}/>
+                                <span className=''>Karachi Branch</span>
 
                             </div>
                         </div>
                         <div className='col-lg-4'>
-                            <div style={{ color: '#DBDBDB' }} className="d-flex pb-3">
-                                <img src={mosqueIcon} alt="mosqueIcon" className="pt-5 pt-lg-0" style={{height: "35"}}/>
-                                <span className='mt-3'>Islamabad Branch</span>
+                            <div style={{ color: '#DBDBDB' }} className="d-flex justify-content-center align-items-baseline">
+                                <img src={mosqueIcon} alt="mosqueIcon" className="mx-2" style={{height: "35"}}/>
+                                <span className=''>Islamabad Branch</span>
 
                             </div>
                         </div>
+
                     </div>
-                    <div className="row">
+                    <div className="row d-flex justify-content-center align-items-center my-4 ">
                         <div className='col-lg-4'>
-                            <div style={{ color: '#DBDBDB' }} className=" d-flex pb-3">
-                                <img src={Map} alt="Map" className="pt-5 pt-lg-0" style={{ height: "27px" }} />
-                                <span className='ml-2'>
-                                    G99P+Q28, Cantt, Lahore,
-                                    Punjab
+                            <div style={{ color: '#DBDBDB' }} className=" d-flex justify-content-center align-items-start ">
+                                <img src={Map} alt="Map" className="mx-2" style={{ height: "24px" }} />
+                                <span className=''>
+                                   G99P+Q28, Cantt, Lahore, Punjab, Pakistan , Near PAF IT <br /> University 
                                 </span>
 
                             </div>
                         </div>
                         <div className='col-lg-4'>
-                            <div style={{ color: '#DBDBDB' }} className="d-flex pb-3">
-                                <img src={Map} alt="Map" className="pt-5 pt-lg-0" style={{ height: "27px" }} />
-                                <span className='ml-2'>
+                            <div style={{ color: '#DBDBDB' }} className="d-flex justify-content-center align-items-start">
+                                <img src={Map} alt="Map" className="mx-2" style={{ height: "24px" }} />
+                                <span className=''>
                                     NASTP Building, Main
                                     Shahrah-e-Faisal Rd,
                                     Faisal Cantonment,
@@ -95,9 +106,9 @@ const Footer = () => {
                             </div>
                         </div>
                         <div className='col-lg-4'>
-                            <div style={{ color: '#DBDBDB' }} className="d-flex pb-3">
-                                <img src={Map} alt="Map" className="pt-5 pt-lg-0" style={{ height: "27px" }} />
-                                <span className='ml-2'>
+                            <div style={{ color: '#DBDBDB' }} className="d-flex justify-content-center align-items-start">
+                                <img src={Map} alt="Map" className="mx-2" style={{ height: "23px" }} />
+                                <span className=''>
                                     J453+GPQ, Old Airport Rd,
                                     Chaklala Cantt., Rawalpindi,
                                     Punjab 46000
@@ -106,41 +117,40 @@ const Footer = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="row">
+                    <div className="row d-flex justify-content-center align-items-center">
                         <div className='col-lg-4'>
-                            <div style={{ color: '#DBDBDB' }} className="d-flex pb-3">
-                                <img src={PhoneIcon} alt="PhoneIcon" className="pt-5 pt-lg-0" style={{ height: "23px" }} />
-                                <span className='ml-2'>
+                            <div style={{ color: '#DBDBDB' }} className="d-flex justify-content-center align-items-center">
+                                <img src={PhoneIcon} alt="PhoneIcon" className="mx-2" style={{ height: "23px" }} />
+                                <span className=''>
                                     03004406163
                                 </span>
 
                             </div>
                         </div>
                         <div className='col-lg-4'>
-                            <div style={{ color: '#DBDBDB' }} className="d-flex pb-3">
-                                <img src={PhoneIcon} alt="PhoneIcon" className="pt-5 pt-lg-0" style={{ height: "23px" }} />
-                                <span className='ml-2'>
+                            <div style={{ color: '#DBDBDB' }} className="d-flex justify-content-center align-items-center">
+                                <img src={PhoneIcon} alt="PhoneIcon" className="mx-2" style={{ height: "23px" }} />
+                                <span className=''>
                                     03332775771
                                 </span>
 
                             </div>
                         </div>
                         <div className='col-lg-4'>
-                            <div style={{ color: '#DBDBDB' }} className="d-flex pb-3">
-                                <img src={PhoneIcon} alt="PhoneIcon" className="pt-5 pt-lg-0" style={{ height: "23px" }} />
-                                <span className='ml-2'>
+                            <div style={{ color: '#DBDBDB' }} className="d-flex justify-content-center align-items-center">
+                                <img src={PhoneIcon} alt="PhoneIcon" className="mx-2" style={{ height: "23px" }} />
+                                <span className=''>
                                     03004406163
                                 </span>
 
                             </div>
                         </div>
                     </div>
-
                 </div>
             </div>
-            <div className="d-none">
+            <div className="container-fluid" >
                 <hr style={{ color: "white", background: "white" }} />
-                <div className="container my-3">
+                <div className="container-fluid ">
                     <div className="row d-flex justify-content-between">
                         <div className="col-12 col-sm-8 text-center text-md-left">
                             COMPLIANCE PRIVACY TERMS TRADEMARK
